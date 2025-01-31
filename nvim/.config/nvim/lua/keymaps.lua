0d4ea5a51f4064307ab1e94c7c46f44dd1cd9e70
eolybq <jez.vasek@gmail.com>
2025-02-01 00:22:08 +0100

    reg nvim + map to nohl

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -1,9 +1,15 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+-- So leader doesnt move cursor
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>')
 
+-- Clear search
+vim.keymap.set("n", "<leader>l", ":nohlsearch<CR>")
+
+-- Exit term mode esc
 vim.keymap.set('t', '<Esc>', "<C-\\><C-n>")
 
+-- Yank and P from os reg
 vim.keymap.set("v", "<C-c>", '"+y') -- Copy
 vim.keymap.set("n", "<C-v>", '"+p') -- Paste normal mode
 vim.keymap.set("i", "<C-v>", '<ESC>"+pla') -- Paste insert mode
