b072b77cb83ce88e3b5d1898de5300181b2ae99a
eolybq <jez.vasek@gmail.com>
2025-01-31 19:15:45 +0100

    none ls py setup, rofi changed

--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -1,15 +1,14 @@
 return {
-  "nvimtools/none-ls.nvim",
-  dependencies = {
-    "nvimtools/none-ls-extras.nvim",
-  },
+	"nvimtools/none-ls.nvim",
+	dependencies = {
+		"nvimtools/none-ls-extras.nvim",
+	},
 	config = function()
 		local null_ls = require("null-ls")
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.stylua,
-        null_ls.builtins.diagnostics.ruff,
-        null_ls.builtins.formatting.black,
+				null_ls.builtins.formatting.black,
 			},
 		})
 		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
