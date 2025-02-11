588c895cae3818047d814364eb7b20eccf9390ad
eolybq <jez.vasek@gmail.com>
2025-02-11 17:06:54 +0100

    border weaybar , rofi

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -22,4 +22,4 @@ vim.keymap.set("v", "<C-c>", '"+y')        -- Copy
 vim.keymap.set("n", "<C-v>", '"+p')        -- Paste normal mode
 vim.keymap.set("i", "<C-v>", '<ESC>"+pla') -- Paste insert mode
 
-vim.keymap.set("n", "<leader>gh", "<cmd>CopilotChat<cr><esc>")
+vim.keymap.set({ "n", "v" }, "<leader>gh", "<cmd>CopilotChat<cr><esc>")
