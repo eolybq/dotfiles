b190e48671affc4de450fb90b63773332763cbc4
eolybq <jez.vasek@gmail.com>
2025-02-11 16:11:01 +0100

    better copilot chat

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -22,13 +22,33 @@ return {
     {
         "CopilotC-Nvim/CopilotChat.nvim",
         dependencies = {
-            { "nvim-lua/plenary.nvim"}, -- for curl, log and async functions
+            { "nvim-lua/plenary.nvim" }, -- for curl, log and async functions
         },
         cmd = "CopilotChat",
-        build = "make tiktoken",                   -- Only on MacOS or Linux
-        opts = {
-            -- See Configuration section for options
-        },
-        -- See Commands section for default commands if you want to lazy load on them
+        build = "make tiktoken", -- Only on MacOS or Linux
+        opts = function()
+            local user = vim.env.USER or "User"
+            user = user:sub(1, 1):upper() .. user:sub(2)
+            return {
+                question_header = "  " .. user .. " ",
+                answer_header = "  Copilot ",
+                window = {
+                    width = 0.3,
+                },
+            }
+        end,
+        config = function(_, opts)
+            local chat = require("CopilotChat")
+
+            vim.api.nvim_create_autocmd("BufEnter", {
+                pattern = "copilot-chat",
+                callback = function()
+                    vim.opt_local.relativenumber = false
+                    vim.opt_local.number = false
+                end,
+            })
+
+            chat.setup(opts)
+        end,
     },
 }
