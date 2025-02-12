191582e7ec8879d2d5444c4f20376da0fbca6b7a
eolybq <jez.vasek@gmail.com>
2025-02-12 18:20:27 +0100

    notify style

--- nvim/.config/nvim/lua/plugins/noice.lua
+++ nvim/.config/nvim/lua/plugins/noice.lua
@@ -1,60 +1,66 @@
 return {
-	"folke/noice.nvim",
-	event = "VeryLazy",
-	opts = {
-		-- add any options here
-	},
-	dependencies = {
-		-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-		"MunifTanjim/nui.nvim",
-		-- OPTIONAL:
-		--   `nvim-notify` is only needed, if you want to use the notification view.
-		--   If not available, we use `mini` as the fallback
-		"rcarriga/nvim-notify",
+	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		opts = {
+			-- add any options here
+		},
+		dependencies = {
+			"MunifTanjim/nui.nvim",
+		},
 		config = function()
-			require("notify").setup({
-				background_colour = "#000000",
+			require("noice").setup({
+				lsp = {
+					-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+					override = {
+						["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+						["vim.lsp.util.stylize_markdown"] = true,
+						["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+					},
+					signature = { enabled = false },
+					-- hover = { enabled = false },
+				},
+
+				presets = {
+					bottom_search = true,
+					long_message_to_split = true,
+				},
+
+				routes = {
+					{
+						filter = {
+							event = "msg_show",
+							any = {
+								{ find = "%d+L, %d+B" },
+								{ find = "; after #%d+" },
+								{ find = "; before #%d+" },
+							},
+						},
+						view = "mini",
+					},
+				},
+
+				cmdline = {
+					format = {
+						shell = { pattern = "^:%s*!", icon = ">_", lang = "zsh" },
+						filter = false,
+					},
+				},
 			})
 		end,
 	},
-	config = function()
-		require("noice").setup({
-			lsp = {
-				-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-				override = {
-					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-					["vim.lsp.util.stylize_markdown"] = true,
-					["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-				},
-				signature = { enabled = false },
-				-- hover = { enabled = false },
-			},
+	{
+		"rcarriga/nvim-notify",
+		config = function()
+			require("notify").setup({
+				background_colour = "#ffffff",
+				timeout = 3000,
+				render = "simple",
 
-			presets = {
-				bottom_search = true,
-				long_message_to_split = true,
-			},
-
-			routes = {
-				{
-					filter = {
-						event = "msg_show",
-						any = {
-							{ find = "%d+L, %d+B" },
-							{ find = "; after #%d+" },
-							{ find = "; before #%d+" },
-						},
-					},
-					view = "mini",
-				},
-			},
-
-			cmdline = {
-				format = {
-					shell = { pattern = "^:%s*!", icon = ">_", lang = "zsh" },
-					filter = false,
-				},
-			},
-		})
-	end,
+				-- on_open = function(win)
+				-- 	vim.api.nvim_win_set_config(win, { focusable = false })
+				-- end,
+			})
+		end,
+	},
 }
